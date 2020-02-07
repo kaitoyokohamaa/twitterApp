1b4c11627da76de35848457643333651be030c4d
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-02-07 16:00:17 +0900

    フェッチでユーザー登録まで

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+const postBtn = document.getElementById('post-btn');
+const user_name=document.getElementById('UserName');
+const user_bio=document.getElementById('User');
+const user_mail=document.getElementById('Email');
+const user_pass=document.getElementById('Password');
+const user_confirmpass=document.getElementById('ConfirmPassword');
+
+    const sendHttpRequest = (method, url, data) => {
+        return fetch(url, {
+          method: method,
+          body: JSON.stringify(data),
+          headers: data ? { 'Content-Type': 'application/json' } : {}
+        }).then(response => {
+          if (response.status >= 400) {
+            // !response.ok
+            return response.json().then(errResData => {
+              const error = new Error('Something went wrong!');
+              error.data = errResData;
+              throw error;
+            });
+          }
+          return response.json();
+        });
+      };
+const sendData = () => {
+    sendHttpRequest('POST', 'https://teachapi.herokuapp.com/sign_up', {
+            "sign_up_user_params": {
+                "name": user_name.value,
+                "bio":  user_bio.value,
+                "email":  user_mail.value,
+                "password": user_pass.value,
+                "password_confirmation":  user_confirmpass.value
+            }
+        })
+        .then(responseData => {
+            console.log(responseData);
+        })
+        .catch(err => {
+            console.log(err, err.data);
+        });
+};
+postBtn.addEventListener('click', sendData);
